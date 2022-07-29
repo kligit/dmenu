69cd42c47edec96705cfbb25e5f87b0437ebc405
kligit <klimail@duck.com>
2022-07-29 11:20:21 +0300

    first commit

---
+++ config.h
@@ -0,0 +1,30 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Noto Sans:size=11:style=regular,microns:size=12:antialias=true,font awesome:size=12:anitalias=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#525252", "#0c0c0d" },
+	[SchemeSel] = { "#d8d8d8", "#0c0c0d" },
+	[SchemeSelHighlight] = { "#2ce882", "#0c0c0d" },
+	[SchemeNormHighlight] = { "#273468", "#0c0c0d" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 0;
+static unsigned int columns    = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 2;
